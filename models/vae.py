c488d272c2f8d2eef8bfcd7d1b27be833774fc4a
Majdj95 <72466345+Majdj95@users.noreply.github.com>
2020-12-23 00:47:56 +0100

    autoencoder

--- models/vae.py
+++ models/vae.py
@@ -1,26 +1,85 @@
 #
 #	Variational Autoencoder
 #
+#	Authors: David, Majd, Paul, Fredrick
+#
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.nn as nn
 import numpy as np
+import torch
+import scipy
 import math
 
 
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#			Helper Functions
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+#			AUTOENCODER
+#
+#	__Note__: This is an intial setup of an autoencoder.
+# 	 The parameters are static and based on dimensions of
+#	 MNIST-dataset. The parameters will be adjusted to work 
+#	with any dataset in the end. 
+#
+#	q_sigma and the third layer in the original decoder
+# 	is defined with NonLinear in utils.nn. This is an attempt
+#	to implement these layers with Tensorflow.
+#
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+#Image Dimensions (28x28), which is the dimension
+#of one data point in the MINST.
+inputSize = [1, 28, 28]
+
+#Doule layered encoder. Takes one image with dimension (784,) as input, x, and returns a (300,) vector. 
+#This process resemles q(z | x) in the graphical representation. 
+encoder = keras.Sequential([
+	keras.layers.Dense(300, input_shape=(np.prod(inputSize)	,), activation='relu', name='EncLayer1'),
+	keras.layers.Dense(300, input_shape=(300,), activation='relu', name='EncLayer2'),
+])
+
+z_size = 40		# 40 stochastic hidden units for z - retrieved from the report p.5
+#mean
+q_mu = keras.Sequential(
+	keras.layers.Dense(z_size, input_shape=(300,))
+)
+#variance
+q_sigma = keras.Sequential(
+	keras.layers.Dense(z_size, input_shape=(300,), activation='tanh')
+)
+
+#Three layered decoder. Input is a sample z, and the decoder returns a (784,) vector.
+#This process resemles p(x | z) in the graphical representation. 
+decoder = keras.Sequential([
+	keras.layers.Dense(300, input_shape=(40, ), activation='relu', name='DecLayer1'),
+	keras.layers.Dense(300, input_shape=(300,), activation='relu', name='DecLayer2'),
+	keras.layers.Dense(np.prod(inputSize), input_shape=(300,), activation ='sigmoid', name='DecNonLinearLayer')
+])
+
+
 
 ##
-##	Computes p(x|z)
+##	Variational posterior
+##
+##	Input:		x			data point
+##
+##	Retruns 	q_z_mean	mean
+##				q_z_var		variance 
 ##
 def p(x):
 	pass
 
 ##
-##	Computes the variational posterior, q(z| x)
+##	Generative posterior
 ##
-def q(x):
+##	Input:		z			sample point
+##
+##	Retruns 	x_mean		mean, which could be interpreted
+##							as the reconstructed image.
+##				x_var		variance. This term is only computed for 
+##							non-binary dataset.
+def q(z):
 	pass
 
 ##
@@ -40,9 +99,7 @@ def loss(x):
 	return loss, RL, KL
 
 ##
-##	Computes the reparameterization trick.
-##
-##	This function is called when computing the variational posterior.
+##	Reparameterization trick.
 ##
 def repTrick():
 	pass
@@ -69,38 +126,3 @@ def prior(z, pType='gaussian'):
 def forwardPass():
 	pass
 
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-#			MODEL
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-##
-##	Encodes data points by computing.
-##
-##	Inputs:	x 					Datapoint(-s)
-##
-##	Return: z = (mu, sigma)		Encoded data points
-##
-def encoder(x):
-	pass
-
-##
-##	Decodes data points by computing.
-##
-##	Inputs:	sample	A distribution sample
-##
-##	Return 	x_hat	Decoded data points
-##
-def decoder(sample):
-	pass
-
-
-def main():
-	"""
-	dataset = tf.keras.datasets.mnist
-	(train_x, train_y), (test_x, test_y) = dataset.load_data()
-	print(train_x.describe())
-	"""
-	pass
-
-main()
