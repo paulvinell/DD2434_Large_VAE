a439939b6c8f5ea41fe325609e2c5721d3c73d6e
David Kragbé <davidkragbe55@gmail.com>
2020-12-27 14:27:30 +0100

    one pass through the model

--- large_vae/experiment/train.py
+++ large_vae/experiment/train.py
@@ -1,9 +1,43 @@
+import tensorflow as tf
+from tensorflow import keras
 
-# Here we train the model omn the train dataset
-def train_model(model, args, train_dataset):
+@tf.function
+def train_step(model, optimizer, x):
+    """ Performs a training step for a set of samples """
 
-    input("Press Enter to begin...")
-    for epoch in range(1, 1 + args.epochs):
-        print("Epoch {}".format(epoch))
-        for x in train_dataset:
-            model.train_step(x)
+    with tf.GradientTape() as tape:
+        loss = model.loss(x)
+
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+    return loss
+
+
+# Here we train the model on the train dataset
+def one_pass(model, dataset, mode = 'evaluate', optimizer = None):
+    """
+        One pass through the model (1 epoch)
+        Can be for training or just for evaluation
+    """
+
+    RE = 0
+    loss = 0
+    KL = 0
+
+    for batch_data in dataset:
+        if mode == 'train':
+            loss_stamp, RE_stamp, KL_stamp = train_step(model, optimizer, batch_data)
+        elif mode == 'evaluate':
+            loss_stamp, RE_stamp, KL_stamp = model.loss(batch_data)
+
+        RE += RE_stamp
+        loss += loss_stamp
+        KL += KL_stamp
+
+    # We average loss, KL and RE over batch size
+    RE /= len(dataset)
+    loss /= len(dataset)
+    KL /= len(dataset)
+
+    return loss, KL, RE
